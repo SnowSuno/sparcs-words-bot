74a857883212b1307849c447f58d4eeb1106dbcd
Soonho Kwon <percy3368@gmail.com>
2023-06-12 02:53:58 +0900

    :sparkles: Format text

--- src/slack.js
+++ src/slack.js
@@ -1,4 +1,17 @@
 import axios from "axios";
 
-export const sendMessage = (text) => axios
-  .post(process.env.SLACK_WEBHOOK_URL, { text });
+export const sendMessage = (text) => axios.post(
+  process.env.SLACK_WEBHOOK_URL,
+  {
+    blocks: [
+      {
+        type: "section",
+        text: { type: "mrkdwn", text: formatText(text) },
+      }
+    ]
+  }
+);
+
+const formatText = (text) => ">*" + text
+  .replaceAll(/\[\[([^\]\[]+)]]/g, "<https://wiki.sparcs.org/index.php/$1>")
+  .replace(" - ", "*\\n>- ");
